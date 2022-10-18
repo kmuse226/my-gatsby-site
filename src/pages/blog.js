68b49e03a1620e3a2ee3e086c214d8cdc1f3a516
junsungkim <jskim2@wemb.co.kr>
2022-10-18 10:44:30 +0900

    part 4 done

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,20 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { graphql }from "gatsby"
 
-const BlogPage = () => {
+const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
     </Layout>
   )
 }
@@ -13,3 +22,13 @@ const BlogPage = () => {
 export const Head = () => <Seo title="My Blog Posts" />
 
 export default BlogPage
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
